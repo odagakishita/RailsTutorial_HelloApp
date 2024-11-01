8e39c5700458a202dbdd22962d3613e2b6f4dac9
odagakishita <theta1736@keio.jp>
2024-11-01 14:38:57 +0900

    適当２

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
     #アクションを定義する
-    def goodmorning
-        render html: "じゃあ横浜にしようニャン"
-    end
-    def hello
-        render html: "¡Hola, mundo!"
-    end
-    def goodbye 
-        render html: "goodbye, world!"
+    def hakase
     end
 end
